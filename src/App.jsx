40fd7e8e4e0ec82f3fd7788b3ef148b3b4a14e0c
raju365 <rajubarman7665@gmail.com>
2025-02-03 20:11:39 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import { setLocalStorage } from "./utils/LocalStorage";
 // import { AuthContext } from "./context/AuthProvider";
 
 function App() {
   // const [user, setUser] = useState(null);
   // const authData = useContext(AuthContext);
   // console.log(authData.emplyees);
+  useEffect(() => {
+    setLocalStorage();
+  });
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
       setUser("admin");
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import AuthContext from "./context/AuthContext.jsx";
 // import AuthProvider from "./context/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <AuthProvider> */}
-    <App />
+    <AuthContext>
+      <App />
+    </AuthContext>
     {/* </AuthProvider> */}
   </StrictMode>
 );
